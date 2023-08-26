3074cdc19555ad9c0d4675b0bc4b06ccf542c7cb
WhalandA <102492629+WhalandA@users.noreply.github.com>
2023-08-26 13:41:14 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-August
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-emx43b)
---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'preact/hooks'
+//import preactLogo from './assets/preact.svg'
+//import viteLogo from '/vite.svg'
+
+import SLMLogo from './assets/slmLogo.svg'
+
+import './app.css'
+
+
+
+export function App() {
+  return (
+    <>
+      <h1>SLM</h1>
+    </>
+  )
+}
